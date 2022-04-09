4d76bd1b608255c4bdc8a0e907691658efbdacfe
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-09 19:13:07 +0600

    login

--- src/Component/LogIn/LogIn.js
+++ src/Component/LogIn/LogIn.js
@@ -1,4 +1,8 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword
+} from "firebase/auth";
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import app from "../../firebase.init";
@@ -6,8 +10,10 @@ import "./LogIn.css";
 const auth = getAuth(app);
 
 const LogIn = () => {
-  const [email, setEmail] = useState([]);
-  const [password, setPassword] = useState([]);
+  const [validated, setValidated] = useState(false);
+  const [registered, setregistered] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -15,20 +21,64 @@ const LogIn = () => {
   const handlePasswordBlur = (event) => {
     setPassword(event.target.value);
   };
+  const handleCheck = (event) => {
+    setregistered(event.target.checked);
+  };
   const handleSubmit = (event) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    setValidated(true);
+
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          emailVarification();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    const handlePasswordReset=()=>{
+      sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent!
+        // ..
+        console.log('reset');
       })
-      .catch((error) => {
-        console.log(error);
+    }
+    const emailVarification = () => {
+      sendEmailVerification(auth.currentUser).then(() => {
+        // Email verification sent!
+        console.log("sent");
+        // ...
       });
+    };
+   
     event.preventDefault();
   };
   return (
     <div className="w-50 mx-auto mt-2 login">
-      <h1 className="text-primary text-center">Log In</h1>
-      <Form onSubmit={handleSubmit}>
+      <h1 className="text-primary text-center">
+        {registered ? "Log In" : "Sign Up"}
+      </h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -36,6 +86,9 @@ const LogIn = () => {
             type="email"
             placeholder="Enter email"
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -45,9 +98,21 @@ const LogIn = () => {
             type="password"
             placeholder="Password"
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            onChange={handleCheck}
+            type="checkbox"
+            label="All ready registered"
+          />
+        </Form.Group>
+        <Button onClick={handlePasswordReset} variant="link">Password Rest</Button>
+        <br />
         <Button variant="primary" type="submit">
-          Log In
+          {registered ? "Log In" : "Sign Up"}
         </Button>
       </Form>
     </div>
